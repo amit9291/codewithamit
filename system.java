6195983dae420e38a8ac8b119f24923f9dd52212
codewithamit <amit9291@gmail.com>
2022-05-26 13:30:16 +0100

    commit

--- system.java
+++ system.java
@@ -8,4 +8,15 @@ public class system {
     System.out.println(s);
         System.out.println("Jiaan");
         System.out.println("push back");
+        StringBuffer sb= new StringBuffer("Hello");
+sb.reverse();
+System.out.println(sb);
+sb.delete(1, 4);
+System.out.println(sb.capacity());
+sb.append(" Java is an Island");
+System.out.println(sb);
+sb.append("git bash");
+System.out.println(sb);
+System.out.println(sb.capacity());
+
 }}
