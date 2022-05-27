f760efde43caf3a5f3933ceddedf2e70f06fe813
codewithamit <amit9291@gmail.com>
2022-05-27 19:36:47 +0100

    commit

--- Practice.java
+++ Practice.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Practice{
 public static void main(String[] args) {
     System.out.println("practice");
-}
-}
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Enter your name : ");
+    scan.nextLine();
+System.out.println("Enter Your Age :");
+int x =scan.nextInt();
+if(x >18){
+System.out.println("You are welcome");
+}else{   
+System.out.println("you are not welcome");
+}}}
