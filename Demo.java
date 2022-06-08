93747bbfff7f158fe3b1289db4316ec11260ff93
codewithamit <amit9291@gmail.com>
2022-06-08 19:53:41 +0100

    push

--- Demo.java
+++ Demo.java
@@ -1,7 +1,28 @@
-import javax.sound.sampled.SourceDataLine;
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Demo");
+      /*  int number=0,reverse=0;
+        System.out.println("Enter any digits and press enter");
+        Scanner scan= new Scanner(System.in);
+        number=scan.nextInt() ;
+        for(;number !=0; number= number/10) {
+int remainder = number % 10;
+         reverse = reverse * 10  +  remainder; 
+        }
+        System.out.println("The reverse of the given number is: "+ reverse );*/
+         int number =0, reversenum = 0;
+         System.out.println("Enter any number and press enter");
+         try (Scanner scan = new Scanner(System.in)) {
+            number=scan.nextInt();
+        }
+         while (number !=0) {
+             reversenum=reversenum*10;
+             reversenum=reversenum+number%10;
+             number=number/10;
+             
+         }
+         System.out.println("Reverse number of input number is :" + reversenum );
+
     }
 }
--- Practice.java
+++ Practice.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Practice{
 public static void main(String[] args) {
     System.out.println("practice");
-    Scanner scan=new Scanner(System.in);
-    System.out.println("Enter your name : ");
-    scan.nextLine();
+    try (Scanner scan = new Scanner(System.in)) {
+        System.out.println("Enter your name : ");
+        scan.nextLine();
 System.out.println("Enter Your Age :");
 int x =scan.nextInt();
 if(x >18){
 System.out.println("You are welcome");
 }else{   
 System.out.println("you are not welcome");
-}}}
+}
+    }}}
