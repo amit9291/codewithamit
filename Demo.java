33e01f5933dbb1f89a6fd8aea6df823209fb4b1e
codewithamit <amit9291@gmail.com>
2022-06-08 20:04:49 +0100

    commit

--- Demo.java
+++ Demo.java
@@ -2,24 +2,29 @@ import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) {
-      /*  int number=0,reverse=0;
+
+    // using for loop 
+      int number=0,reverse=0;
         System.out.println("Enter any digits and press enter");
-        Scanner scan= new Scanner(System.in);
-        number=scan.nextInt() ;
+        try (Scanner scan = new Scanner(System.in)) {
+            number=scan.nextInt() ;
+        }
         for(;number !=0; number= number/10) {
 int remainder = number % 10;
          reverse = reverse * 10  +  remainder; 
         }
-        System.out.println("The reverse of the given number is: "+ reverse );*/
-         int number =0, reversenum = 0;
+        System.out.println("The reverse of the given number is: "+ reverse );
+// using while loop 
+
+         int num =0, reversenum = 0;
          System.out.println("Enter any number and press enter");
-         try (Scanner scan = new Scanner(System.in)) {
-            number=scan.nextInt();
+         try (Scanner in = new Scanner(System.in)) {
+            num=in.nextInt();
         }
-         while (number !=0) {
-             reversenum=reversenum*10;
-             reversenum=reversenum+number%10;
-             number=number/10;
+         while (num !=0) {
+             reversenum=reversenum * 10;
+             reversenum=reversenum + num % 10;
+             num=num / 10;
              
          }
          System.out.println("Reverse number of input number is :" + reversenum );
